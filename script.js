27e249254f3f78410c4c1a25d8e1b382f5e3b1da
MicheleSaraceni <michele.saraceni23@email.com>
2024-10-29 14:23:31 +0100

    added for and if and ended exercise

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ Abbiamo visto qualcosa di particolare che possiamo usare?
 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 2. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 Buon lavoro e buon divertimento! :faccia_leggermente_sorridente: */
+
+
+
+//Creo un for che con ta fino a cento e ad ognoi giro controllo il valore di i se è 3/5 o entrambi tramite if ed in caso stampo la parola corrispondente
+
+for (i = 1; i <= 100; i++){
+    
+    if (i % 3 === 0 && i % 5 === 0){
+        //Controllo per prima cosa se il valore è multiplo sia di 3 che di 5 perche raggionando a cascata se dovesse essere vera prima una delle due condizioni singole non entrerebbe in questa sezione
+        console.log ("(" + i + ") FizzBuzz");
+    } else if (i % 3 === 0){
+        //Controllo se il numero è divisibile per 3 e stampo Fizz
+        console.log ("(" + i + ") Fizz");
+    } else if (i % 5 === 0){
+        //Controllo se il numero è divisibile per 5 e stampo Buzz
+        console.log ("(" + i + ") Buzz");
+    } else{
+        //Se tutte le condizioni precedenti sono falze stampo solamente il numero ricorrente del ciclo
+        console.log (i);
+    }
+}
